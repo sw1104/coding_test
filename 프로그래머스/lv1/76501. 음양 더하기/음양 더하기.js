4c87e01be7b1192cd9e07714f0876b428b0509d5
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-22 22:25:33 +0900

    Update 음양 더하기.js

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,3 +1,15 @@
 function solution(ab, s) {
+  // 첫 풀이
+  //    let arr = [];
+  //    let answer = 0;
+  //    for (let i=0; i<ab.length; i++){
+  //        if(s[i] === true){
+  //            arr.push(ab[i])
+  //        } else if(s[i] === false){
+  //            arr.push(-ab[i]) 
+  //        }
+  //         answer += arr[i]
+  //     } return answer
+  // 두번 째 풀이
     return ab.reduce((acc, value, i) => acc + (value * (s[i] ? 1 : -1)) ,0)
 }

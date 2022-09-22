1e5ddc439b587b344f69a04caa3bf71d097210b8
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-22 22:18:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.09 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -1,12 +1,3 @@
 function solution(ab, s) {
-  let arr = [];
-  let answer = 0;
-    for (let i=0; i<ab.length; i++){
-      if(s[i] === true){
-        arr.push(ab[i])
-      } else if(s[i] === false){
-        arr.push(-ab[i]) 
-      }
-      answer += arr[i]
-    } return answer
+    return ab.reduce((acc, value, i) => acc + (value * (s[i] ? 1 : -1)) ,0)
 }

8eca2710bf1cb61a1627e90011d08a162e19b807
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-24 13:59:20 +0900

    Update 수박수박수박수박수박수？.js

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,3 +1,17 @@
 function solution(n) {
+    // 첫 번째 풀이
+//     let str = "수박".split("")
+//     let str1 = str[0];
+//   	let str2 = str[1];
+//     let arr = [];
+//   for (let i = 1; i <= n; i++){
+//   		if(i % 2 === 1){
+//         arr.push(str1)
+//       } else {
+//         arr.push(str2)
+//       }
+//       }
+//     return arr.join("")
+    // 첫 번째 풀이 후 repeat이 떠올라서 두번 째 풀이
     return "수박".repeat(n).slice(0,n);
 }

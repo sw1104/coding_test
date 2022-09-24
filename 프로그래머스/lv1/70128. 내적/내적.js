e8ba994f39c4e3ee91705bc319b2cc074863397e
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-24 17:58:38 +0900

    [level 1] Title: 내적, Time: 0.11 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -2,8 +2,8 @@ function solution(a, b) {
   let arr = [];
   let count = 0
 	for (let i = 0; i < a.length; i++) {
-      arr.push(a[i] * b[i])
-    count += arr[i]
+      
+    count += a[i] * b[i]
     }
   return count
 }

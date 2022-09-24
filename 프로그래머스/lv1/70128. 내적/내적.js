8ba4b1c576670d4733e7ce4d4410d4119ed42df6
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-24 18:03:23 +0900

    Update 내적.js

--- 프로그래머스/lv1/70128. 내적/내적.js
+++ 프로그래머스/lv1/70128. 내적/내적.js
@@ -1,3 +1,12 @@
 function solution(a, b) {
+    // 첫 풀이
+//     let arr = [];
+//     let count = 0
+// 	for (let i = 0; i < a.length; i++) {
+//         count += a[i] * b[i]
+//     }
+//     return count
+    
+    // reduce를 이용한 풀이
     return a.reduce((acc, a, i) => acc += a*b[i] ,0)
 }

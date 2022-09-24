29480e66b05c64685646a997c1238b0a6fecca6a
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-09-24 13:57:45 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,14 +1,3 @@
 function solution(n) {
-    let str = "수박".split("")
-    let str1 = str[0];
-  	let str2 = str[1];
-    let arr = [];
-  for (let i = 1; i <= n; i++){
-  		if(i % 2 === 1){
-        arr.push(str1)
-      } else {
-        arr.push(str2)
-      }
-      }
-    return arr.join("")
+    return "수박".repeat(n).slice(0,n);
 }

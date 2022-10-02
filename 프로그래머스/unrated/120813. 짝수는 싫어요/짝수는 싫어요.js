b7f2e5749fc5c715222833f62b0c06f3eaa8cdd6
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-02 21:22:27 +0900

    [unrated] Title: 짝수는 싫어요, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let arr = [];
+    for (let i = 1; i <= n; i++){
+        if(i % 2 === 1){
+            arr.push(i)
+        }
+    }
+    return arr.sort((x, y) => x - y)
+}

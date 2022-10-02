57ef7ad3bc93b1feda8253b6a7f06b71d6e9810f
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-02 13:41:11 +0900

    [unrated] Title: 머쓱이보다 키 큰 사람, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.js
@@ -0,0 +1,8 @@
+function solution(array, height) {
+    let arr = [];
+    for( let i = 0; i < array.length; i++ ){
+        if( array[i] > height){
+            arr.push(array[i])
+        }
+    } return arr.length
+}

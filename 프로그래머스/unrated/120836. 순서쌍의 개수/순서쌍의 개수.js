030eec6d75820644479300dbe77d9bf966e3e2a9
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-04 21:42:21 +0900

    [unrated] Title: 순서쌍의 개수, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120836. 순서쌍의 개수/순서쌍의 개수.js
@@ -0,0 +1,10 @@
+function solution(n) {
+    let count = 0;
+    let arr = [];
+    for(let i = 1; i <= n; i++){
+        if(n % i === 0){
+            arr.push(i)
+        }
+    }
+    return arr.length
+}

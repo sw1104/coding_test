638a30436103363b3c74fbf4712d2aa7cf042a8d
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-07 07:49:35 +0900

    [Bronze V] Title: 시험 성적, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,12 @@
+result = int(input())
+
+if 90 <= result <= 100:
+    print("A")
+elif 80 <= result <= 89:
+    print("B")
+elif 70 <= result <= 79:
+    print("C")
+elif 60 <= result <= 69:
+    print("D")
+else:
+    print("F")

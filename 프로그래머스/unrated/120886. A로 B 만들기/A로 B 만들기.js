9ce40737e186ddc57b1a31e4f41ab27967b4535a
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-09 10:52:23 +0900

    [unrated] Title: A로 B 만들기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
+++ 프로그래머스/unrated/120886. A로 B 만들기/A로 B 만들기.js
@@ -1,8 +1,6 @@
 function solution(before, after) {
     let a = before.split('').sort();
     let b = after.split('').sort();
-    console.log(a)
-    console.log(b)
     let count = 0
     for(let i = 0; i < a.length; i++){
         if(a[i] === b[i]){

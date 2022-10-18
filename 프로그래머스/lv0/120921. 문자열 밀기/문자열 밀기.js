040d632219d58b4f8440e871e689133702a4dac9
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-18 12:06:41 +0900

    [level 0] Title: 문자열 밀기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
+++ 프로그래머스/lv0/120921. 문자열 밀기/문자열 밀기.js
@@ -1,13 +1,15 @@
-function solution(A, B) {
-    let as = A.split('')
-    if(A === B) {
-        return 0
-    }
-    for(let i = 1; i <= A.length; i++){
-        let aa = as.unshift(as.pop())
-        if(as.join('') === B){
-            return i
-        }
-    }
-    return -1
-}
+// function solution(A, B) {
+//     let as = A.split('')
+//     if(A === B) {
+//         return 0
+//     }
+//     for(let i = 1; i <= A.length; i++){
+//         let aa = as.unshift(as.pop())
+//         if(as.join('') === B){
+//             return i
+//         }
+//     }
+//     return -1
+// }
+
+const solution = (a, b) => (b + b).indexOf(a)

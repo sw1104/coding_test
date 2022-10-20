88de95b43b29f25d1050555f13ef613d839125ab
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-10-20 17:32:18 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,15 @@
+function solution(keyinput, board) {    
+    let result = [0, 0]
+    for(let i = 0; i < keyinput.length; i++){
+        if(keyinput[i] === 'up' && result[1]+1 <= parseInt(board[1]/2)){
+            result[1] += 1
+        } else if(keyinput[i] === 'down' && result[1]-1 >= -parseInt(board[1]/2)){
+            result[1] -= 1
+        } else if(keyinput[i] === 'left' && result[0]-1 >= -parseInt(board[0]/2)){
+            result[0] -= 1
+        } else if(keyinput[i] === 'right' && result[0]+1 <= parseInt(board[0]/2)){
+            result[0] += 1
+        }
+    }
+    return result
+}

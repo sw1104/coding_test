c861d9f1f2c7a9f63006996022ee9bf5ef972961
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-11-11 20:58:33 +0900

    [Bronze V] Title: 행렬 덧셈, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -0,0 +1,15 @@
+a, b =[],[]
+n, m = map(int, input().split())
+
+for i in range(n):
+  i = list(map(int, input().split()))
+  a.append(i)
+
+for i in range(n):
+  i = list(map(int, input().split()))
+  b.append(i)
+
+for i in range(n):
+  for j in range(m):
+    print(a[i][j] + b[i][j], end=' ')
+  print()

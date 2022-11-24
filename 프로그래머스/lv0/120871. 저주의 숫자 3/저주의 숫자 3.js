3bc66d04df438b3899d292d6af521de395dac64a
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-11-24 11:35:00 +0900

    [level 0] Title: 저주의 숫자 3, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120871. 저주의 숫자 3/저주의 숫자 3.js
@@ -0,0 +1,9 @@
+function solution(n) {
+    let x = 0
+    for(let i = 1; i <= n; i++){
+        x++
+        while ((x+'').includes('3') || x % 3 === 0){
+            x++
+        }
+    } return x
+}

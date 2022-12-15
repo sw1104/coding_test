2c982ac444c384410feef1bb01c49e54db4f709d
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-15 12:19:13 +0900

    [level 0] Title: 이진수 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
+++ 프로그래머스/lv0/120885. 이진수 더하기/이진수 더하기.js
@@ -1,8 +1,32 @@
 function solution(bin1, bin2) {
     let a = parseInt(bin1, 2)
     let b = parseInt(bin2, 2)
-    console.log(a)
     
     let add = a + b
     return add.toString(2)
 }
+
+// const solution = (bin1, bin2) => {
+//     function translate(bin){
+//     if (bin !=="0"){
+//         const Arr = []
+//         const bin1Arr = [...bin].reverse()
+
+//         for (let i=0; i<bin1Arr.length; i++){
+//             bin1Arr[i] === "1" ? Arr.push(i) : Arr.push("a")
+//         }
+
+//         const map = Arr.map(x=> Math.pow(2,x))
+//         const filter = map.filter(x=> Number(x))
+//         const result = filter.reduce((a,b)=>a+b)
+
+//         return Number(result)
+//         }
+//         return 0
+//     }
+
+//     const sum = translate(bin1) + translate(bin2)
+    
+//     return sum.toString(2);
+    
+// }

da7ff2891cf72df9acbe07d743b34156e43fbc32
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-24 17:08:52 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,10 +1,23 @@
+// function solution(keyinput, board) {    
+//     let result = [0, 0]
+//     for(let i = 0; i < keyinput.length; i++){
+//         if(keyinput[i] === 'up' && result[1]+1 <= parseInt(board[1]/2)) result[1] += 1
+//         if(keyinput[i] === 'down' && result[1]-1 >= -parseInt(board[1]/2)) result[1] -= 1
+//         if(keyinput[i] === 'left' && result[0]-1 >= -parseInt(board[0]/2)) result[0] -= 1 
+//         if(keyinput[i] === 'right' && result[0]+1 <= parseInt(board[0]/2)) result[0] += 1
+//     }
+//     return result
+// }
+
 function solution(keyinput, board) {    
-    let result = [0, 0]
+    const boardLimit = board.map((x) => Math.floor(x / 2));
+    let x = 0
+    let y = 0
     for(let i = 0; i < keyinput.length; i++){
-        if(keyinput[i] === 'up' && result[1]+1 <= parseInt(board[1]/2)) result[1] += 1
-        if(keyinput[i] === 'down' && result[1]-1 >= -parseInt(board[1]/2)) result[1] -= 1
-        if(keyinput[i] === 'left' && result[0]-1 >= -parseInt(board[0]/2)) result[0] -= 1 
-        if(keyinput[i] === 'right' && result[0]+1 <= parseInt(board[0]/2)) result[0] += 1
+        if(keyinput[i] === 'up' && y+1 <= boardLimit[1]) y += 1
+        if(keyinput[i] === 'down' && y-1 >= -boardLimit[1]) y -= 1
+        if(keyinput[i] === 'left' && x-1 >= -boardLimit[0]) x -= 1 
+        if(keyinput[i] === 'right' && x+1 <= boardLimit[0]) x += 1
     }
-    return result
+    return [x,y]
 }

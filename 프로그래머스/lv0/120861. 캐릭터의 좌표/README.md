870ca590828c573530a6b69c130ac57139504aca
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-24 16:28:42 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120861. 캐릭터의 좌표/README.md
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/README.md
@@ -1,10 +1,10 @@
 # [level 0] 캐릭터의 좌표 - 120861 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120861) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.08 ms
+메모리: 33.5 MB, 시간: 0.06 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -1,15 +1,10 @@
 function solution(keyinput, board) {    
     let result = [0, 0]
     for(let i = 0; i < keyinput.length; i++){
-        if(keyinput[i] === 'up' && result[1]+1 <= parseInt(board[1]/2)){
-            result[1] += 1
-        } else if(keyinput[i] === 'down' && result[1]-1 >= -parseInt(board[1]/2)){
-            result[1] -= 1
-        } else if(keyinput[i] === 'left' && result[0]-1 >= -parseInt(board[0]/2)){
-            result[0] -= 1
-        } else if(keyinput[i] === 'right' && result[0]+1 <= parseInt(board[0]/2)){
-            result[0] += 1
-        }
+        if(keyinput[i] === 'up' && result[1]+1 <= parseInt(board[1]/2)) result[1] += 1
+        if(keyinput[i] === 'down' && result[1]-1 >= -parseInt(board[1]/2)) result[1] -= 1
+        if(keyinput[i] === 'left' && result[0]-1 >= -parseInt(board[0]/2)) result[0] -= 1 
+        if(keyinput[i] === 'right' && result[0]+1 <= parseInt(board[0]/2)) result[0] += 1
     }
     return result
 }

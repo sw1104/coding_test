1e84e829f8c29dc7e4941cd12e773a3f98bd9ded
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-27 12:10:34 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,11 @@
+const solution = (strings, n) => {
+  let result = strings.sort((a,b) => {
+    if(a[n] > b[n]) return 1;
+    if(a[n] < b[n]) return -1;
+    if(a[n] === b[n]){
+      if(a > b) return 1;
+      if(a < b) return -1;
+    }
+  })
+  return result
+}

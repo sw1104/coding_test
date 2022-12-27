457723960ddf5a3c3f8a0ebf169b23bf6ada312a
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-27 12:45:23 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 9.07 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
+++ 프로그래머스/lv1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -1,11 +1,15 @@
-const solution = (strings, n) => {
-  let result = strings.sort((a,b) => {
-    if(a[n] > b[n]) return 1;
-    if(a[n] < b[n]) return -1;
-    if(a[n] === b[n]){
-      if(a > b) return 1;
-      if(a < b) return -1;
-    }
-  })
-  return result
+// const solution = (strings, n) => {
+//   let result = strings.sort((a,b) => {
+//     if(a[n] > b[n]) return 1;
+//     if(a[n] < b[n]) return -1;
+//     if(a[n] === b[n]){
+//       if(a > b) return 1;
+//       if(a < b) return -1;
+//     }
+//   })
+//   return result
+// }
+
+const solution = (s, n) => {
+    return s.sort((a,b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]))
 }

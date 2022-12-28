e25f9cc497df0fe361391a23123698b8efea718d
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-28 22:04:23 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.07 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -7,11 +7,49 @@
 //     return sol2(ck)
 // }
 
-const solution = ch => {
-  let count = 0;
-  while(ch >= 10) {
-    count = count + Math.floor(ch / 10)
-    ch = Math.floor(ch/10) + (ch%10)
-  }
-  return count
+// const solution = ch => {
+//   let count = 0;
+//   while(ch >= 10) {
+//     count = count + Math.floor(ch / 10)
+//     ch = Math.floor(ch/10) + (ch%10)
+//   }
+//   return count
+// }
+function solution(chicken) {
+    let Arr = 0 // 몫의 합
+    let Arr2 = 0 // 나머지의 합
+
+    const self = (chicken) => {
+        
+        if (chicken < 10) {
+            if(Arr2 + chicken >= 10){
+              return Arr +  Math.floor(Arr2/10) + 1
+                // if(Arr2%10 === 9){
+                //     return Arr +  Math.floor(Arr2/10) + 1
+                // }
+                // if(Arr2%10 < 9){
+                //     return Arr + Math.floor(Arr2/10)
+                // }
+            }
+            return Arr
+        }
+        
+        if(chicken >= 10){
+            let a = Math.floor(chicken/10)
+            let b = chicken%10
+            Arr = Arr + a
+            Arr2 = Arr2 + b
+            return self(a)
+          }
+    }
+    
+    if (chicken < 10){
+        return 0
+    }
+    if(chicken === 10){
+        return 1
+    }
+    if(chicken>10){
+        return self(chicken)
+    }
 }

3209ab7e4a52bc7b54aae8a41e3bcc9a377e5424
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2022-12-28 20:37:37 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
+++ 프로그래머스/lv0/120884. 치킨 쿠폰/치킨 쿠폰.js
@@ -1,9 +1,17 @@
-const solution = ck => {
-    const sol2 = cp => {
-        if(cp < 10) return 0
-        const sv = Math.floor(cp / 10)
-        return sv + sol2(sv + (cp % 10));
-    }
-    return sol2(ck)
-}
+// const solution = ck => {
+//     const sol2 = cp => {
+//         if(cp < 10) return 0
+//         const sv = Math.floor(cp / 10)
+//         return sv + sol2(sv + (cp % 10));
+//     }
+//     return sol2(ck)
+// }
 
+const solution = ch => {
+  let count = 0;
+  while(ch >= 10) {
+    count = count + Math.floor(ch / 10)
+    ch = Math.floor(ch/10) + (ch%10)
+  }
+  return count
+}

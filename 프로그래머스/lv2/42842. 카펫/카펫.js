66b15ac5168d4d43d3a05adcf8c14e9fdc283720
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-08 14:29:51 +0900

    [level 2] Title: 카펫, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.js
+++ 프로그래머스/lv2/42842. 카펫/카펫.js
@@ -6,12 +6,11 @@ function solution(brown, yellow) {
             arr.push(i)
         }
     }
-    for (let i = 0; i < arr.length; i++) {
-        if(arr.length % 2 === 1) {
-            if(parseInt(arr.length / 2) === i)
-                return [arr[i],arr[i]]
+    if(arr.length % 2 === 1) {
+                return [arr[parseInt(arr.length / 2)],arr[parseInt(arr.length / 2)]]
         }
-        if(arr.length % 2 === 0) {
+    if(arr.length % 2 === 0) {
+        for (let i = 0; i < arr.length; i++) {
             for(let j = 0; j < arr.length; j++) {
             if(arr[i] * arr[j] === by && (arr[i]-2) * (arr[j]-2) === yellow)
                 return [arr[j], arr[i]]

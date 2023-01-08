fb3f5fcc847d6d508d71067241fcb83b7b057fe2
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-08 15:43:00 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.js
@@ -6,7 +6,6 @@ function solution(s) {
   for(let i = 0; i < a.length; i++){
     arr.push(a[i].split(''));
     }
-  console.log(arr)
   for(let i = 0; i < arr.length; i++){
       for(let j = 0; j < arr[i].length; j++){
         if(j % 2 === 0){

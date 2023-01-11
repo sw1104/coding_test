13344615496113956e967868a5f10a4f37442982
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-11 16:13:20 +0900

    [level 0] Title: 특이한 정렬, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
+++ 프로그래머스/lv0/120880. 특이한 정렬/특이한 정렬.js
@@ -1,12 +1,15 @@
 function solution(nl, n) {
     const result = nl.sort((a, b) => {
-        const x = a - n;
-        const y = b - n;
-        if(Math.abs(x) === Math.abs(y)){
+        let x = a - n
+        let y = b - n
+        if(Math.abs(x) === Math.abs(y)) {
             return b - a
         } else {
-           return Math.abs(x) - Math.abs(y)
+            return Math.abs(x) - Math.abs(y)
         }
     })
     return result
 }
+
+
+

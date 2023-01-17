d18d8608f5ba5310d00d68f62d541a96b386039e
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-17 14:15:57 +0900

    [level 4] Title: [3차] 자동완성, Time: 565.21 ms, Memory: 178 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/17685. ［3차］ 자동완성/［3차］ 자동완성.js
@@ -0,0 +1,30 @@
+function solution(words) {
+    let result = 0;
+    const trie = makeTrie(words);
+    for (const word of words) {
+        let count = 0;
+        let current = trie;
+        for(const [index, letter] of [...word].entries()) {
+            count += 1;
+            if(current[letter][0] <= 1) {
+                break;
+            }
+            current = current[letter][1];
+        }
+        result += count;
+    }
+    return result;
+}
+
+function makeTrie(words) {
+    const root = {};
+    for (const word of words) {
+        let current = root;
+        for(const letter of word) {
+            if(!current[letter]) current[letter] = [0, {}];
+            current[letter][0] = 1 + (current[letter][0] || 0);
+            current = current[letter][1];
+        }
+    }
+    return root;
+}

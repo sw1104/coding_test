e7d7538815b8ea1c1bee9854316c9b31288f979d
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-18 14:06:00 +0900

    [level 1] Title: 2016년, Time: 0.12 ms, Memory: 33.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12901. 2016년/2016년.js
@@ -0,0 +1,7 @@
+function solution(a, b) {
+    const week = ['SUN','MON','TUE','WED','THU','FRI','SAT'];
+    
+    const dayOfWeek = week[new Date('2016-'+a + '-'+b).getDay()];
+
+    return dayOfWeek;
+}

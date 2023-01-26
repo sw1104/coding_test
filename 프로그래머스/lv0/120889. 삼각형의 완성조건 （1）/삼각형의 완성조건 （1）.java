858673594e453ff650a38739e1f79c9308f22c4c
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-01-26 16:03:03 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.49 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -1,15 +1,24 @@
+// import java.util.Arrays;
+
+// class Solution {
+//     public int solution(int[] sides) {
+//         Arrays.sort(sides);
+//         int sum = 0;
+//         for(int i = 0; i < sides.length - 1; i ++) {
+//             sum += sides[i];
+//         }
+//         if(sum > sides[sides.length - 1]) {
+//             return 1;
+//         }
+//         return 2;
+//     }
+// }
+
 import java.util.Arrays;
 
 class Solution {
     public int solution(int[] sides) {
         Arrays.sort(sides);
-        int sum = 0;
-        for(int i = 0; i < sides.length - 1; i ++) {
-            sum += sides[i];
-        }
-        if(sum > sides[sides.length - 1]) {
-            return 1;
-        }
-        return 2;
+        return sides[2] < sides[0] + sides[1] ? 1 : 2;
     }
 }

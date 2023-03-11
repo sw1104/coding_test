b2c10be3023c475fb8118ee934a2df305c63b001
Lee Sang Woo <105622759+sw1104@users.noreply.github.com>
2023-03-11 17:23:55 +0900

    [level 2] Title: 3월에 태어난 여성 회원 목록 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/131120. 3월에 태어난 여성 회원 목록 출력하기/3월에 태어난 여성 회원 목록 출력하기.sql
@@ -0,0 +1,1 @@
+select MEMBER_ID, MEMBER_NAME, GENDER, DATE_FORMAT(DATE_OF_BIRTH, '%Y-%m-%d') as DATE_OF_BIRTH from MEMBER_PROFILE where DATE_OF_BIRTH like '%-03-%' and GENDER = 'w' and TLNO is not null order by MEMBER_ID;
